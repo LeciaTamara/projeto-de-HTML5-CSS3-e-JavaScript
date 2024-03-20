637e8ee40d1cf6b706ff41f3f416dc9e08364be5
Lécia Tamara <139869948+LeciaTamara@users.noreply.github.com>
2024-03-20 09:08:39 -0300

    commit projeto completo

---
+++ site2/javascript.js
@@ -0,0 +1,9 @@
+//tela de login
+function longar() {
+    window.location = "filmes.html"
+}
+
+// botão voltar das páginas dos filmes para a página filme
+function votar() {
+    window.location = "filmes.html"
+}
